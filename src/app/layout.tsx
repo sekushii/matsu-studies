e71faccc1b97c97e2a19cbd02e4765c01e75efed
sekushii <76856870+sekushii@users.noreply.github.com>
2025-05-15 19:59:31 -0300

    fix header position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,16 +28,18 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${geist.variable}`}>
-        <header className="flex h-16 items-center justify-end gap-4 p-4">
-          <SignedOut>
-            <SignInButton />
-            <SignUpButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
-        <body>{children}</body>
+        <body>
+          <header className="flex h-16 items-center justify-end gap-4 p-4">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
